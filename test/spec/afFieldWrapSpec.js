7a2cce37ed3be8d019f789ffb3d8f0d3094af8d3
Frank van Wijk <fvanwijk@xebia.com>
2015-07-16 15:11:20 +0200

    We expect that when a form component with isolate scope is created, the afFieldWrap attribute is set on the same element as the directive. To prevent the formController from being inaccessible by the child scopes. the formController set on the scope again by the afFieldWrap directive.

--- test/spec/afFieldWrapSpec.js
+++ test/spec/afFieldWrapSpec.js
@@ -1,20 +1,35 @@
 describe('afFieldWrap', function () {
-  var $rootScope;
+
+  beforeAll(function () {
+    angular.module('angularFormMessages').directive('isolate', function () {
+      return {
+        // fieldWrap is in a template to make sure that these elements are in the isolate scope
+        template: '<div af-field-wrap="user.name"></div>',
+        // Create new isolate scope to demonstrate that the formController is inaccessible in the afFieldWrap
+        scope: {}
+      };
+    });
+  });
 
   beforeEach(function () {
     mox
       .module('angularFormMessages')
       .run();
 
-    inject(function (_$rootScope_) {
-      $rootScope = _$rootScope_;
-    });
-
     createScope();
-    compileHtml('<div af-field-wrap="user.name"></div>', this.$scope);
+
+    compileHtml('<form name="userForm" af-submit>' +
+      '<div isolate></div>' +
+    '</form>', this.$scope);
   });
 
   it('should set the modelPath in the controller from the directive attribute', function () {
-    expect(this.element.controller('afFieldWrap').messageId).toBe('user.name');
+    expect(this.element.find('[af-field-wrap]').controller('afFieldWrap').messageId).toBe('user.name');
+  });
+
+  it('should put the formController on the scope', function () {
+    var innerScope = this.element.find('[af-field-wrap]').scope();
+    expect(innerScope.$id).not.toBe(this.$scope.$id);
+    expect(innerScope.userForm).toBeDefined();
   });
 });
