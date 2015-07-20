8ee73dfea92197dc164e74eeb0499bcea4b2f7a1
Frank van Wijk <fvanwijk@xebia.com>
2015-07-20 12:08:35 +0200

    Do not request a custom template, instead implement a custom directive that shows the error component

--- src/afError.js
+++ src/afError.js
@@ -1,6 +1,5 @@
 angular.module('angularFormMessages').directive('afError', function () {
   return {
-    templateUrl: 'templates/error.html',
     require: '^afFieldWrap',
     scope: true,
     link: function linkFn($scope, elem, attrs, ctrl) {
