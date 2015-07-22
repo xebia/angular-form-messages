14627f0c534b401e3bd9298df7067abb488ef2c4
Frank van Wijk <fvanwijk@xebia.com>
2015-07-23 00:25:41 +0200

    Add a messageService with a method to determine the message type with highest severity for a list of messages

---
+++ src/messageService.js
@@ -0,0 +1,21 @@
+angular.module('angularFormMessages')
+  .constant('MESSAGE_TYPES', ['SUCCESS', 'INFO', 'WARNING', 'ERROR'])
+  .factory('MessageService', function (MESSAGE_TYPES) {
+    return {
+      /**
+       * Determine the message type with the highest severity from a list of messages
+       * @param {Object[]} messages
+       * @returns {string} message type with the highest severity
+       */
+      determineMessageType: function (messages) {
+        var severityIndex = -1;
+        angular.forEach(messages, function (message) {
+          var index = MESSAGE_TYPES.indexOf(message.type);
+          if (index > severityIndex) {
+            severityIndex = index;
+          }
+        });
+        return severityIndex === -1 ? undefined : MESSAGE_TYPES[severityIndex];
+      }
+    };
+  });
