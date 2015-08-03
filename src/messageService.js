769aeba94fe90e8ba5c5dc9358df7a88b86d3379
Frank van Wijk <fvanwijk@xebia.com>
2015-08-03 07:23:44 +0200

    Transform MessageService into provider.

--- src/messageService.js
+++ src/messageService.js
@@ -1,33 +1,33 @@
 angular.module('angularFormMessages')
   .constant('MESSAGE_TYPES', ['SUCCESS', 'INFO', 'WARNING', 'ERROR'])
-  .factory('MessageService', function (
-    $rootScope,
+  .provider('MessageService', function (
     MESSAGE_TYPES
   ) {
-    return {
-      /**
-       * Determine the message type with the highest severity from a list of messages
-       * @param {Object[]} messages
-       * @returns {string} message type with the highest severity
-       */
-      determineMessageType: function (messages) {
-        var severityIndex = -1;
-        angular.forEach(messages, function (message) {
-          var index = MESSAGE_TYPES.indexOf(message.type);
-          if (index > severityIndex) {
-            severityIndex = index;
-          }
-        });
-        return severityIndex === -1 ? undefined : MESSAGE_TYPES[severityIndex];
-      },
+    this.$get = function ($injector) {
+      return {
+        /**
+         * Determine the message type with the highest severity from a list of messages
+         * @param {Object[]} messages
+         * @returns {string} message type with the highest severity
+         */
+        determineMessageType: function (messages) {
+          var severityIndex = -1;
+          angular.forEach(messages, function (message) {
+            var index = MESSAGE_TYPES.indexOf(message.type);
+            if (index > severityIndex) {
+              severityIndex = index;
+            }
+          });
+          return severityIndex === -1 ? undefined : MESSAGE_TYPES[severityIndex];
+        },
 
-      validation: function (messageId, callback) {
-
-        $rootScope.$on('validation', function (event, validationMessageId, messages, messageType) {
-          if (validationMessageId === messageId) {
-            callback(messages, messageType);
-          }
-        });
-      }
+        validation: function (messageId, callback) {
+          $injector.get('$rootScope').$on('validation', function (event, validationMessageId, messages, messageType) {
+            if (validationMessageId === messageId) {
+              callback(messages, messageType);
+            }
+          });
+        }
+      };
     };
   });
