c26389d5d151a2578fc6cbdd21cc55c1a7357ef9
Frank van Wijk <fvanwijk@xebia.com>
2015-08-03 07:42:08 +0200

    Bump version to v0.7.0

--- dist/angular-form-messages.js
+++ dist/angular-form-messages.js
@@ -6,7 +6,7 @@ angular.module('angularFormMessages').directive('afField', ["$rootScope", "MESSA
   MessageService
 ) {
   return {
-    //priority: 100,
+    priority: 100,
     require: ['ngModel', 'afField', '^afSubmit', '^form'],
     controller: function () {
       function setMessageDetails(type) {
@@ -36,7 +36,7 @@ angular.module('angularFormMessages').directive('afField', ["$rootScope", "MESSA
         afField = ctrls[1],
         submit = ctrls[2],
         form = ctrls[3],
-        triggerOn = attrs.afTriggerOn || submit.triggerOn || 'change',
+        triggerOn = attrs.afTriggerOn || submit.triggerOn || MessageService.triggerOn(),
         isPristineAfterSubmit;
 
       /**
@@ -209,34 +209,44 @@ angular.module('angularFormMessages').directive('afSubmitButton', function () {
 
 angular.module('angularFormMessages')
   .constant('MESSAGE_TYPES', ['SUCCESS', 'INFO', 'WARNING', 'ERROR'])
-  .factory('MessageService', ["$rootScope", "MESSAGE_TYPES", function (
-    $rootScope,
+  .provider('MessageService', ["MESSAGE_TYPES", function (
     MESSAGE_TYPES
   ) {
-    return {
-      /**
-       * Determine the message type with the highest severity from a list of messages
-       * @param {Object[]} messages
-       * @returns {string} message type with the highest severity
-       */
-      determineMessageType: function (messages) {
-        var severityIndex = -1;
-        angular.forEach(messages, function (message) {
-          var index = MESSAGE_TYPES.indexOf(message.type);
-          if (index > severityIndex) {
-            severityIndex = index;
-          }
-        });
-        return severityIndex === -1 ? undefined : MESSAGE_TYPES[severityIndex];
-      },
+    var triggerOn = 'change';
 
-      validation: function (messageId, callback) {
-
-        $rootScope.$on('validation', function (event, validationMessageId, messages, messageType) {
-          if (validationMessageId === messageId) {
-            callback(messages, messageType);
-          }
-        });
-      }
+    this.setTriggerOn = function (newValue) {
+      triggerOn = newValue;
     };
+
+    this.$get = ["$injector", function ($injector) {
+      return {
+        /**
+         * Determine the message type with the highest severity from a list of messages
+         * @param {Object[]} messages
+         * @returns {string} message type with the highest severity
+         */
+        determineMessageType: function (messages) {
+          var severityIndex = -1;
+          angular.forEach(messages, function (message) {
+            var index = MESSAGE_TYPES.indexOf(message.type);
+            if (index > severityIndex) {
+              severityIndex = index;
+            }
+          });
+          return severityIndex === -1 ? undefined : MESSAGE_TYPES[severityIndex];
+        },
+
+        validation: function (messageId, callback) {
+          $injector.get('$rootScope').$on('validation', function (event, validationMessageId, messages, messageType) {
+            if (validationMessageId === messageId) {
+              callback(messages, messageType);
+            }
+          });
+        },
+
+        triggerOn: function () {
+          return triggerOn;
+        }
+      };
+    }];
   }]);
