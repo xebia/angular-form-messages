5eaffce8a256c10279c27a93e8a3d6607c788bfe
Frank van Wijk <fvanwijk@xebia.com>
2015-08-17 17:05:07 +0200

    Made the afFieldState directive more complete. The 'has-...' class is updated every 'validation' event, The class that is added is based on the alerts in the child element. If there are no, look at the messages (if the event's messageId is the same as the messageId of the fieldState) and determine the 'has-...' class. If there are still no messages, show the 'has-success' class when showSuccess is true. Specs are cleaned up as well.

--- src/bootstrap/afFieldState.js
+++ src/bootstrap/afFieldState.js
@@ -5,27 +5,89 @@ angular.module('angularFormMessagesBootstrap')
  * When there is no message (the field is valid) and showSuccess is true, show the 'has-success' class.
  */
   .directive('afFieldState', function (
+    $timeout,
     AfMessageService,
     MESSAGE_TYPES
   ) {
     return {
       require: '^afSubmit',
+      controller: angular.noop,
       link: function ($scope, elem, attrs, afSubmitCtrl) {
-        var messageId = attrs.afFieldState || attrs.afMessageId;
+        function typeToFieldStateClass(messageType) {
+          var mapping = {};
+          mapping[MESSAGE_TYPES[0]] = 'has-success';
+          mapping[MESSAGE_TYPES[1]] = 'has-info';
+          mapping[MESSAGE_TYPES[2]] = 'has-warning';
+          mapping[MESSAGE_TYPES[3]] = 'has-error';
+          return mapping[messageType]
+        }
 
-        AfMessageService.validation($scope, messageId, function (validationMessageId, messages, messageType) {
-          angular.forEach(MESSAGE_TYPES, function (type) {
-            attrs.$removeClass('has-' + type.toLowerCase());
+        function alertClassToType(alertClass) {
+          return {
+            'alert-success': MESSAGE_TYPES[0],
+            'alert-info': MESSAGE_TYPES[1],
+            'alert-warning': MESSAGE_TYPES[2],
+            'alert-danger': MESSAGE_TYPES[3]
+          }[alertClass];
+        }
+
+        function addClass(messageType) {
+          attrs.$addClass(typeToFieldStateClass(messageType));
+        }
+
+        function removeClass(messageType) {
+          attrs.$removeClass(typeToFieldStateClass(messageType));
+        }
+
+        function groupByMessageType(alerts) {
+          var groupedAlerts = {};
+          angular.forEach(alerts, function (alert) {
+            // Find the alert-* class, expects one found class
+            var type = alert.className.match(/(alert-\w+)/g)[0];
+            groupedAlerts[type] = alert;
           });
+          return groupedAlerts;
+        }
 
-          if (messages.length) {
-            // FIX ME: count the visible messages in the DOM because 'messages' only says something about the new messages in this event
-            if (!(AfMessageService.showMultiple() && messages.length > 1)) {
-              attrs.$addClass('has-' + messageType.toLowerCase());
+        var messageId = attrs.afFieldState || attrs.afMessageId;
+        var hasValidClass;
+
+        $scope.$on('validation', function (event, validationMessageId, messages) {
+          // Make sure that the AfMessages validation callback runs first
+          $timeout(function () {
+            var alerts = elem[0].querySelectorAll('.alert');
+            var alertClasses = Object.keys(groupByMessageType(alerts));
+
+            // Remove all classes to start clean
+            angular.forEach(MESSAGE_TYPES, function (type) {
+              removeClass(type);
+            });
+
+            // There is exactly one afMessage in the DOM
+            if (alertClasses.length === 1) {
+              addClass(alertClassToType(alertClasses[0]));
             }
-          } else if (afSubmitCtrl.showSuccess) {
-            attrs.$addClass('has-success');
-          }
+
+            // There is no afMessage but the field with the same messageId has one or more messages
+            if (!alertClasses.length) {
+              if (validationMessageId === messageId) {
+                if (messages.length) {
+                  addClass(AfMessageService.getMostSevereMessage(messages).type);
+                }
+                // There are no afMessages or messages for the current messageId
+                // Determine if there should be a 'success' class for the field with the same message id
+                else if (afSubmitCtrl.showSuccess) {
+                  hasValidClass = true;
+                  addClass(MESSAGE_TYPES[0]);
+                }
+              }
+              // Show the 'success' class when there are no afMessages and the event is not for this fieldstate
+              else if (hasValidClass) {
+                addClass(MESSAGE_TYPES[0]);
+              }
+            }
+
+          });
         });
       }
     };
--- test/spec/bootstrap/afFieldStateSpec.js
+++ test/spec/bootstrap/afFieldStateSpec.js
@@ -1,21 +1,31 @@
 describe('afFieldState', function () {
 
-  function setup(messageType, messageCount) {
-    if (messageType) {
-      this.element.fieldState().removeClass('has-' + messageType.toLowerCase());
-    }
-    this.element.fieldState().addClass(messageType === inj.MESSAGE_TYPES[3] ? 'has-warning has-info has-success' : 'has-error');
-
-    var messages = _.map(_.range(messageCount), function (i) {
-      return {
-        message: 'Message ' + i,
-        type: inj.MESSAGE_TYPES[i]
-      };
+  function setup(messageTypes) {
+    // Add messages
+    this.$scope.messages = _.map(messageTypes, function (type) {
+      return type === inj.MESSAGE_TYPES[3] ? 'danger' : type.toLowerCase();
     });
-
-    // The messageId is not checked because that is mocked away, so can be undefined
-    this.$scope.$emit('validation', undefined, messages, messageType);
     this.$scope.$digest();
+
+    // Add 'has-...' classes that will be removed by the directive
+    var fieldState = this.element.fieldState();
+    fieldState.addClass('has-warning has-info has-success has-error');
+    if (messageTypes.length) {
+      _.each(messageTypes, function (messageType) {
+        fieldState.removeClass('has-' + messageType.toLowerCase());
+      });
+    } else {
+      fieldState.removeClass('has-success');
+    }
+
+    // Send the validation event to trigger the directive
+    validate.call(this);
+  }
+
+  function validate(messageId, messages) {
+    this.$scope.$emit('validation', messageId || 'user.name', messages || []);
+    this.$scope.$digest();
+    inj.$timeout.flush();
   }
 
   function checkMessageClass(className, invert) {
@@ -23,51 +33,27 @@ describe('afFieldState', function () {
     if (invert) {
       ex = ex.not;
     }
-    ex.toHaveClass(className);
+    ex.toHaveClass('has-' + className);
   }
 
   var
     inj,
 
-    errorSetup = _.partial(setup, 'ERROR', 1),
-    warningSetup = _.partial(setup, 'WARNING', 1),
-    infoSetup = _.partial(setup, 'INFO', 1),
-    successSetup = _.partial(setup, 'SUCCESS', 1),
-    noMessageSetup = _.partial(setup, false, 0),
-    successInfoSetup = _.partial(setup, 'INFO', 2),
-
-    expectHasError = _.partial(checkMessageClass, 'has-error'),
-    expectHasWarning = _.partial(checkMessageClass, 'has-warning'),
-    expectHasInfo = _.partial(checkMessageClass, 'has-info'),
-    expectHasSuccess = _.partial(checkMessageClass, 'has-success'),
-
-    expectHasNoError = _.partial(checkMessageClass, 'has-error', true),
-    expectHasNoWarning = _.partial(checkMessageClass, 'has-warning', true),
-    expectHasNoInfo = _.partial(checkMessageClass, 'has-info', true),
-    expectHasNoSuccess = _.partial(checkMessageClass, 'has-success', true);
+    expectHasNoError = _.partial(checkMessageClass, 'error', true),
+    expectHasNoWarning = _.partial(checkMessageClass, 'warning', true),
+    expectHasNoInfo = _.partial(checkMessageClass, 'info', true),
+    expectHasNoSuccess = _.partial(checkMessageClass, 'success', true);
 
   beforeEach(function () {
     mox
       .module('angularFormMessagesBootstrap')
-      .mockServices('AfMessageService')
-      .setupResults(function () {
-        return {
-          AfMessageService: {
-            showMultiple: true,
-            validation: function ($scope, messageId, callback) {
-              // This method is quite hard to mock, so we mimic the implementation, except for the messageId condition
-              $scope.$on('validation', function (event, validationMessageId, messages, messageType) {
-                callback(validationMessageId, messages, messageType);
-              });
-            }
-          }
-        };
-      })
       .run();
 
-    inj = mox.inject('$rootScope', 'MESSAGE_TYPES');
+    inj = mox.inject('$rootScope', '$timeout', 'MESSAGE_TYPES');
     createScope();
-    addSelectors(compileHtml('<form name="userForm" af-submit><div af-field-state="user.name"></div></form>'), {
+    addSelectors(compileHtml('<form name="userForm" af-submit><div af-field-state="user.name">' +
+                             '<div class="alert alert-{{message}}" ng-repeat="message in messages"></div>' +
+                             '</div></form>'), {
       fieldState: '[af-field-state]'
     });
   });
@@ -78,120 +64,140 @@ describe('afFieldState', function () {
       this.element.controller('afSubmit').showSuccess = value;
     }
 
-    it('should register the validation event listener via the AfMessageService', function () {
-      expect(mox.get.AfMessageService.validation).toHaveBeenCalledWith(this.$scope, 'user.name', jasmine.any(Function));
-    });
-
-    describe('when the messageId is passed via the messageId attribute', function () {
-      beforeEach(function () {
-        mox.get.AfMessageService.validation.calls.reset();
-        compileHtml('<form name="userForm" af-submit><div af-field-state af-message-id="user.name"></div></form>');
-      });
-
-      it('should register the validation event listener via the AfMessageService', function () {
-        expect(mox.get.AfMessageService.validation).toHaveBeenCalledWith(this.$scope, 'user.name', jasmine.any(Function));
-      });
-    });
-
     describe('when showSuccess is true on the afSubmit directive', function () {
       beforeEach(_.partial(showSuccess, true));
 
-      describe('when the validation is "valid"', function () {
-        beforeEach(noMessageSetup);
+      describe('when there is no message', function () {
+        beforeEach(_.partial(setup, []));
 
-        it('should add the "has-success" class to the element', expectHasSuccess);
+        it('should add the "has-success" class to the element', _.partial(checkMessageClass, 'success'));
         it('should remove the "has-error" class from the element', expectHasNoError);
         it('should remove the "has-warning" class from the element', expectHasNoWarning);
         it('should remove the "has-info" class from the element', expectHasNoInfo);
-      });
 
-      describe('when the validation is "error"', function () {
-        beforeEach(errorSetup);
-
-        it('should add the "has-error" class to the element', expectHasError);
-        it('should remove the "has-warning" class from the element', expectHasNoWarning);
-        it('should remove the "has-info" class from the element', expectHasNoInfo);
-        it('should remove the "has-success" class from the element', expectHasNoSuccess);
-      });
-
-      describe('when the validation is "warning"', function () {
-        beforeEach(warningSetup);
-
-        it('should add the "has-warning" class to the element', expectHasWarning);
-        it('should remove the "has-error" class from the element', expectHasNoError);
-        it('should remove the "has-info" class from the element', expectHasNoInfo);
-        it('should remove the "has-success" class from the element', expectHasNoSuccess);
-      });
-
-      describe('when the validation is "info"', function () {
-        beforeEach(infoSetup);
-
-        it('should add the "has-info" class to the element', expectHasInfo);
-        it('should remove the "has-error" class from the element', expectHasNoError);
-        it('should remove the "has-warning" class from the element', expectHasNoWarning);
-        it('should remove the "has-success" class from the element', expectHasNoSuccess);
-      });
-
-      describe('when the validation is "success"', function () {
-        beforeEach(successSetup);
-
-        it('should add the "has-success" class to the element', expectHasSuccess);
-        it('should remove the "has-error" class from the element', expectHasNoError);
-        it('should remove the "has-warning" class from the element', expectHasNoWarning);
-        it('should remove the "has-info" class from the element', expectHasNoInfo);
-      });
-
-      describe('when there are multiple validations', function () {
-
-        describe('when we allow to show multiple messages', function () {
-
+        describe('when there is another validation event for another messageId', function () {
           beforeEach(function () {
-            mox.get.AfMessageService.showMultiple.and.returnValue(true);
-            successInfoSetup.call(this);
+            validate.call(this, 'user.other', [{ type: inj.MESSAGE_TYPES[3] }]);
           });
-
-          it('should remove the "has-warning" class from the element', expectHasNoInfo);
-          it('should remove the "has-error" class from the element', expectHasNoError);
-          it('should remove the "has-warning" class from the element', expectHasNoWarning);
-          it('should remove the "has-success" class from the element', expectHasNoSuccess);
+          it('should not remove the "has-success" class from the element', _.partial(checkMessageClass, 'success'));
         });
 
-        describe('when we allow only the message with the highest severity', function () {
+        describe('when there is another validation event for the same messageId', function () {
           beforeEach(function () {
-            mox.get.AfMessageService.showMultiple.and.returnValue(false);
-            successInfoSetup.call(this);
+            validate.call(this, 'user.name', [{ type: inj.MESSAGE_TYPES[3] }]);
+          });
+          it('should remove the "has-success" class from the element', expectHasNoSuccess);
+          it('should add the "has-error" class to the element', _.partial(checkMessageClass, 'error'));
+        });
+      });
+
+      describe('when there is an "error" message', function () {
+        beforeEach(function () {
+          setup.call(this, [inj.MESSAGE_TYPES[3]]);
+        });
+
+        it('should add the "has-error" class to the element', _.partial(checkMessageClass, 'error'));
+        it('should remove the "has-warning" class from the element', expectHasNoWarning);
+        it('should remove the "has-info" class from the element', expectHasNoInfo);
+        it('should remove the "has-success" class from the element', expectHasNoSuccess);
+
+        // This block of course also applies to the other message types
+        describe('when there is another validation event for the same messageId', function () {
+          beforeEach(function () {
+            validate.call(this, 'user.name', [{ type: inj.MESSAGE_TYPES[2] }]);
           });
 
-          it('should add the "has-info" class to the element', expectHasInfo);
-          it('should remove the "has-error" class from the element', expectHasNoError);
-          it('should remove the "has-warning" class from the element', expectHasNoWarning);
-          it('should remove the "has-success" class from the element', expectHasNoSuccess);
+          // Message is ignored since there are still afMessages in the DOM
+          it('should keep the "has-error" class to the element', _.partial(checkMessageClass, 'error'));
+          it('should not add the "has-warning" class to the element', expectHasNoWarning);
+
+          describe('and the messages are gone', function () {
+            beforeEach(function () {
+              this.$scope.messages = [];
+              validate.call(this, 'user.name', [{ type: inj.MESSAGE_TYPES[2] }]);
+            });
+            it('should remove the "has-error" class from the element', expectHasNoError);
+            it('should not add the "warning" class to the element', _.partial(checkMessageClass, 'warning'));
+          });
         });
+      });
+
+      describe('when there is a "warning" message', function () {
+        beforeEach(function () {
+          setup.call(this, [inj.MESSAGE_TYPES[2]]);
+        });
+
+        it('should add the "has-warning" class to the element', _.partial(checkMessageClass, 'warning'));
+        it('should remove the "has-error" class from the element', expectHasNoError);
+        it('should remove the "has-info" class from the element', expectHasNoInfo);
+        it('should remove the "has-success" class from the element', expectHasNoSuccess);
+      });
+
+      describe('when there is an "info" message', function () {
+        beforeEach(function () {
+          setup.call(this, [inj.MESSAGE_TYPES[1]]);
+        });
+
+        it('should add the "has-info" class to the element', _.partial(checkMessageClass, 'info'));
+        it('should remove the "has-error" class from the element', expectHasNoError);
+        it('should remove the "has-warning" class from the element', expectHasNoWarning);
+        it('should remove the "has-success" class from the element', expectHasNoSuccess);
+      });
+
+      describe('when there is a "success" message', function () {
+        beforeEach(function () {
+          setup.call(this, [inj.MESSAGE_TYPES[0]]);
+        });
+
+        it('should add the "has-success" class to the element', _.partial(checkMessageClass, 'success'));
+        it('should remove the "has-error" class from the element', expectHasNoError);
+        it('should remove the "has-warning" class from the element', expectHasNoWarning);
+        it('should remove the "has-info" class from the element', expectHasNoInfo);
+      });
+
+      describe('when there are multiple messages', function () {
+
+        beforeEach(function () {
+          setup.call(this, [inj.MESSAGE_TYPES[1], inj.MESSAGE_TYPES[0]]);
+        });
+
+        it('should remove the "has-warning" class from the element', expectHasNoInfo);
+        it('should remove the "has-error" class from the element', expectHasNoError);
+        it('should remove the "has-warning" class from the element', expectHasNoWarning);
+        it('should remove the "has-success" class from the element', expectHasNoSuccess);
+
       });
     });
 
     describe('when showSuccess is false on the afSubmit directive', function () {
       beforeEach(_.partial(showSuccess, false));
 
-      describe('when the validation is "valid"', function () {
-        beforeEach(noMessageSetup);
+      describe('when there are no messages', function () {
+        beforeEach(_.partial(setup, []));
 
+        it('should not add the "has-error" class', expectHasNoError);
+        it('should not add the "has-warning" class', expectHasNoWarning);
+        it('should not add the "has-info" class', expectHasNoInfo);
         it('should not add the "has-success" class', expectHasNoSuccess);
       });
 
-      describe('when the validation is "error"', function () {
-        beforeEach(errorSetup);
+      describe('when there is an "error" message', function () {
+        beforeEach(function () {
+          setup.call(this, [inj.MESSAGE_TYPES[3]]);
+        });
 
-        it('should add the "has-error" class', expectHasError);
+        it('should add the "has-error" class', _.partial(checkMessageClass, 'error'));
         it('should remove the "has-success" class', expectHasNoSuccess);
         it('should remove the "has-info" class', expectHasNoInfo);
         it('should remove the "has-warning" class', expectHasNoWarning);
       });
 
-      describe('when the validation is "success"', function () {
-        beforeEach(successSetup);
+      describe('when there is a "success" message', function () {
+        beforeEach(function () {
+          setup.call(this, [inj.MESSAGE_TYPES[0]]);
+        });
 
-        it('should add the "has-success" class', expectHasSuccess);
+        it('should add the "has-success" class', _.partial(checkMessageClass, 'success'));
         it('should remove the "has-info" class', expectHasNoInfo);
         it('should remove the "has-warning" class', expectHasNoWarning);
         it('should remove the "has-error" class', expectHasNoError);
