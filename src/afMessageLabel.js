4e8c74876479fc126e1f61f8c4afcb15eb36d3d7
Frank van Wijk <fvanwijk@xebia.com>
2016-02-10 21:56:03 +0100

    Rename specificLabel to validatorSpecificLabel

--- src/afMessageLabel.js
+++ src/afMessageLabel.js
@@ -22,16 +22,16 @@ angular.module('angularFormMessages')
             formCtrl = ctrls[0],
             afMessagesCtrl = ctrls[1],
             fieldName = afMessagesCtrl.fieldName || afMessagesCtrl.fieldNamePrefix,
-            specificLabel = $interpolate(formCtrl.$name)($scope) + (fieldName ? '.' + fieldName : '') + '.' + newVal,
+            validatorSpecificLabel = $interpolate(formCtrl.$name)($scope) + (fieldName ? '.' + fieldName : '') + '.' + newVal,
             genericLabel = AfMessageService.getGenericLabelPrefix() + newVal;
 
-          $translate(specificLabel)
+          $translate(validatorSpecificLabel)
             .then(translate)
             ['catch'](function () {
               $translate(genericLabel)
                 .then(translate)
                 ['catch'](function () {
-                  $log.warn('Missing label: \'' + specificLabel + '\' (specific) or \'' + genericLabel + '\' (generic)');
+                  $log.warn('Missing label: \'' + validatorSpecificLabel + '\' (specific) or \'' + genericLabel + '\' (generic)');
                   translate(newVal);
                 });
             });
