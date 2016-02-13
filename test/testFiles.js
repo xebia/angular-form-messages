9ff5445de0fd4ec5e458ddfe165ab8aa1a1013cb
Frank van Wijk <fvanwijk@xebia.com>
2016-02-13 16:22:01 +0100

    Move all bower dependencies to package.json because bower is dead.

--- test/testFiles.js
+++ test/testFiles.js
@@ -2,12 +2,12 @@ module.exports = [
   {
     type: 'lib',
     files: [
-      'bower_components/lodash/lodash.js',
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
-      'bower_components/mox/dist/mox.js'
+      'node_modules/lodash/lodash.js',
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/jasmine-jquery/lib/jasmine-jquery.js',
+      'node_modules/angular-mox/dist/mox.js'
     ]
   },
   {
